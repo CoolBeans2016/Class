0f5a6630f0b57917884efba4b84e7f6312eff4fa
God Of War <koolbeansmarketing@gmail.com>
2016-04-21 12:21:42 -0400

    God of War is an action-adventure video game series loosely based on Greek mythology.
    The cow jumped over the moon.

--- God of War/Temperature.py
+++ God of War/Temperature.py
@@ -1,2 +1,8 @@
-# God of War is an action-adventure video game series loosely based on Greek mythology.
-'''The cow jumped over the moon.'''
+def main():
+
+    for i in range(1, 100):
+        celsius=i*5
+        fahrenheit=(1.8*celsius+32)
+        print(celsius,"C is equal to",fahrenheit,"F")
+
+main()
